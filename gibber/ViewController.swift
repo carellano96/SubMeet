a9012340c26fa0dc201774b21d95ff649a6d7873
carlos arellano <carlos@carloss-mbp-3.home>
2018-09-12 23:11:37 -0400

    Initial Commit

---
+++ gibber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gibber
+//
+//  Created by carlos arellano on 9/12/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
